8273020d1e86e1e55aacdbcce153a2dce68b43cc
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-09-09 22:26:25 +0530

    day-34

--- leetcode-js/day34.js
+++ leetcode-js/day34.js
@@ -28,7 +28,13 @@ Example 2:
 Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
 Output: [[1,2,3,4],[5,6,7],[8,9,10]]
 Explanation:
-The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.*/
+The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+
+
+
+
+
+*/
 
 
 // Definition for singly-linked list.
