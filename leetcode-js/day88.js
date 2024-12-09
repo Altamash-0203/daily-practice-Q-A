27db43dc0d3678ecb5e051630b619a6ad2e3f80f
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-12-09 22:28:37 +0530

    day-88

---
+++ leetcode-js/day88.js
@@ -0,0 +1,90 @@
+/*
+3152. Special Array II
+Solved
+Medium
+Topics
+Companies
+Hint
+An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+You are given an array of integer nums and a 2D integer matrix queries, where for queries[i] = [fromi, toi] your task is to check that 
+subarray
+ nums[fromi..toi] is special or not.
+
+Return an array of booleans answer such that answer[i] is true if nums[fromi..toi] is special.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,1,2,6], queries = [[0,4]]
+
+Output: [false]
+
+Explanation:
+
+The subarray is [3,4,1,2,6]. 2 and 6 are both even.
+
+Example 2:
+
+Input: nums = [4,3,1,6], queries = [[0,2],[2,3]]
+
+Output: [false,true]
+
+Explanation:
+
+The subarray is [4,3,1]. 3 and 1 are both odd. So the answer to this query is false.
+The subarray is [1,6]. There is only one pair: (1,6) and it contains numbers with different parity. So the answer to this query is true.
+*/
+
+
+///////////////////////// Template ///////////////////////////////
+function Bisect() {
+    return { insort_right, insort_left, bisect_left, bisect_right }
+    function insort_right(a, x, lo = 0, hi = null) {
+        lo = bisect_right(a, x, lo, hi);
+        a.splice(lo, 0, x);
+    }
+    function bisect_right(a, x, lo = 0, hi = null) { // > upper_bound
+        if (lo < 0) throw new Error('lo must be non-negative');
+        if (hi == null) hi = a.length;
+        while (lo < hi) {
+            let mid = parseInt((lo + hi) / 2);
+            a[mid] > x ? hi = mid : lo = mid + 1;
+        }
+        return lo;
+    }
+    function insort_left(a, x, lo = 0, hi = null) {
+        lo = bisect_left(a, x, lo, hi);
+        a.splice(lo, 0, x);
+    }
+    function bisect_left(a, x, lo = 0, hi = null) { // >= lower_bound
+        if (lo < 0) throw new Error('lo must be non-negative');
+        if (hi == null) hi = a.length;
+        while (lo < hi) {
+            let mid = parseInt((lo + hi) / 2);
+            a[mid] < x ? lo = mid + 1 : hi = mid;
+        }
+        return lo;
+    }
+}
+////////////////////////////////////////////////////////
+
+const isArraySpecial = (a, queries) => {
+    let n = a.length, L = [], R = [], res = [], bi = new Bisect();
+    for (let i = 1; i < n; i++) {
+        if (a[i - 1] % 2 == a[i] % 2) {
+            L.push(i - 1);
+            R.push(i);
+        }
+    }
+    for (const [l, r] of queries) { // [L[idx], R[idx]] is subset of [l, r], not special
+        let idx = bi.bisect_left(L, l);
+        if (l < r && l <= L[idx] && r >= L[idx] + 1) { // R[idx] == L[idx] + 1
+            res.push(false);
+        } else {
+            res.push(true);
+        }
+    }
+    return res;
+};
